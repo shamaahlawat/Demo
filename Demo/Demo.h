b8c1677e4da0ac9c43e691a30711f0efda9085a3
Shama Ahlawat <shama@oriserve.com>
2018-12-14 12:47:10 +0530

    service.swift file added

---
+++ Demo/Demo.h
@@ -0,0 +1,19 @@
+//
+//  Demo.h
+//  Demo
+//
+//  Created by Shama Ahlawat on 14/12/18.
+//  Copyright © 2018 oriserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Demo.
+FOUNDATION_EXPORT double DemoVersionNumber;
+
+//! Project version string for Demo.
+FOUNDATION_EXPORT const unsigned char DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo/PublicHeader.h>
+
+
